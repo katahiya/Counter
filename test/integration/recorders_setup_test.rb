4ec0d02951560a41f45f69518c679d2f567af6fb
katahiya <manoshitanoaka@gmail.com>
2017-01-31 14:46:29 +0900

    Test valid setup information

--- test/integration/recorders_setup_test.rb
+++ test/integration/recorders_setup_test.rb
@@ -5,13 +5,44 @@ class RecordersSetupTest < ActionDispatch::IntegrationTest
   test "invalid setup information" do
     get setup_path
     assert_no_difference 'Recorder.count' do
-      post setup_path, params: { recorder: { title: "",
-                                             option: { name: "hoge" } } }
+      assert_no_difference 'Option.count' do
+        post setup_path, params: { recorder: { title: "",
+                                               options_attributes: { "0" => { name: "hoge",
+                                                                          _destroy: false } } } }
+      end
     end
     assert_template 'recorders/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
 
-  
+  test "valid setup information" do
+    get setup_path
+    assert_difference 'Recorder.count', 1 do
+      assert_difference 'Option.count', 1 do
+        post setup_path, params: { recorder: { title: "example",
+                                               options_attributes: { "0" => { name: "hoge",
+                                                                          _destroy: false } } } }
+      end
+    end
+    follow_redirect!
+    assert_template 'recorders/show'
+  end
+
+  test "register multiple options" do
+    get setup_path
+    assert_difference 'Recorder.count', 1 do
+      assert_difference 'Option.count', 3 do
+        post setup_path, params: { recorder: { title: "example",
+                                               options_attributes: { "0" => { name: "hoge",
+                                                                          _destroy: false },
+                                                                     "1" => { name: "piyo",
+                                                                          _destroy: false },
+                                                                     "2" => { name: "fuga",
+                                                                          _destroy: false } } } }
+      end
+    end
+    follow_redirect!
+    assert_template 'recorders/show'
+  end
 end
