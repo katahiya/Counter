3d1041c227eba1e90325b2b6eab0aa4e3057e9e0
katahiya <menoshitanoaka@gmail.com>
2017-02-14 19:11:17 +0900

    Enable login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post '/setup', to: 'recorders#create'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :recorders
