ee06ce3dc3c01a28679b363e2e03f455a4c606da
katahiya <menoshitanoaka@gmail.com>
2017-02-21 16:17:24 +0900

    Modify record table

--- test/integration/recorders_profile_test.rb
+++ test/integration/recorders_profile_test.rb
@@ -6,19 +6,41 @@ class RecordersProfileTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:hoge)
     @recorder = recorders(:hoge)
+    @isolated_recorder = recorders(:isolated)
   end
 
-  test "profile display" do
+  test "profile display with any records" do
     log_in_as(@user)
     get recorder_path(@recorder)
     assert_template 'recorders/show'
     assert_select 'title', full_title(@recorder.title)
     assert_select 'h1', text: @recorder.title
     @recorder.options.each do |option|
-      assert_match option.name, response.body
+      assert_select "form input[data-disable-with=?]", option.name
     end
+    assert_select 'h2', text: "no records", count: 0
+    assert_select 'h2', text: "Records"
+    assert_select 'div.table-responsive'
+    assert_select 'div.records'
     @recorder.records.each do |record|
-      assert_match record.data, response.body
+      assert_select "table td.data", text: record.data
+      assert_select 'a[href=?]', record_path(record), text: 'delete'
     end
   end
+
+  test "profile display with no records" do
+    log_in_as(@user)
+    get recorder_path(@isolated_recorder)
+    assert_template 'recorders/show'
+    assert_select 'title', full_title(@isolated_recorder.title)
+    assert_select 'h1', text: @isolated_recorder.title
+    @isolated_recorder.options.each do |option|
+      assert_select "form input[data-disable-with=?]", option.name
+    end
+    assert_select 'h2', text: "no records"
+    assert_select 'h2', text: "Records", count: 0
+    assert_select 'div.table-responsive', count: 0
+    assert_select 'div.records', count: 0
+  end
+
 end
