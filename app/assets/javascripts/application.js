4bad65169777befa7cd65c72b739d6bd1e2fc3b7
katahiya <menoshitanoaka@gmail.com>
2017-03-14 16:56:55 +0900

    recordの全選択と全選択解除を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,46 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
 $(function() {
-  $('#options-etc').popover({
+
+  //checkbox
+  $('.check_all').click(function(){
+    $('input[name="ids[]"]').prop('checked', true);
+    generate_buttons();
+  });
+
+  $(document).on('click', '.uncheck_all', function(){
+    $('input[name="ids[]"]').prop('checked', false);
+    remove_buttons();
+  });
+
+  $('input[name="ids[]"]').change(function(){
+    checked_any();
+  });
+
+  //popover
+  $('.hover_popover').popover({
     trigger: 'hover', // click,hover,focus,manualを選択出来る
   });
+
+  //private
+  var uncheck_all_button = '<button name="button" type="button" class="uncheck_all">全選択解除</button>'
+  var generate_buttons = function() {
+    if($('.uncheck_all').length > 0) return;
+    $('.appear_checked').append(uncheck_all_button);
+    $('.plural_action_button').removeAttr('disabled');
+  };
+  var remove_buttons = function() {
+    $('.appear_checked').empty();
+    $('.plural_action_button').attr('disabled', 'disabled');
+  };
+  var checked_any = function() {
+    if($('input[name="ids[]"]:checked').length > 0){
+      generate_buttons();
+    }
+    else{
+      remove_buttons();
+    }
+  };
 });
