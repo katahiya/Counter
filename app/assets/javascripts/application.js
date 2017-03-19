cc6fa35a4f2c20dd39df1e778f53e56f8d095e33
katahiya <menoshitanoaka@gmail.com>
2017-03-19 22:57:41 +0900

    記録テーブルにスクロールを追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,41 @@ var resize_windowful = function() {
   var window_height = $(window).height();
   var header_height = $('.navbar-fixed-top').height();
   var resize = window_height - header_height - 50;
-  $('.windowful').css("height", resize + "px");
-  var side_height = $('.option-bar-head').height();
-  console.log(window_height);
-  console.log(header_height);
-  console.log(side_height);
-  $('.record_form').css("height", resize-side_height + "px");
+  $('.windowful').css("max-height", resize + "px");
+  resize_option_bar(resize);
+  resize_records_table(resize + 30);
 };
+var resize_option_bar = function(base) {
+  if($('.option-bar').width() < 125){
+    $('.options-col').css("visibility", "hidden");
+    return;
+  }
+  $('.options-col').css("visibility", "visible");
+  var side_height = $('.option-bar-head').height();
+  $('.record_form').css("max-height", base-side_height + "px");
+}
+var resize_records_table = function(base) {
+  var head_height = $('.records-head').height();
+  var buttons_height = $('.records-buttons').height();
+  var thead_height = $('.records-thead').height();
+  var table_width = $('.records-tbody').width();
+  console.log(head_height);
+  console.log(buttons_height);
+  console.log(thead_height);
+  console.log(table_width);
+  $('.records-tbody').css("max-height", base-head_height-buttons_height-thead_height + "px");
+  $('td.record-select').css("width", table_width*0.1 + "px");
+  $('td.record-action').css("width", table_width*0.1 + "px");
+  $('td.record-no').css("width", table_width*0.1 + "px");
+  $('td.record-data').css("width", table_width*0.6 + "px");
+  $('td.record-count').css("width", table_width*0.1 + "px");
+  table_width -= 20;
+  $('th.record-select').css("width", table_width*0.1 + "px");
+  $('th.record-action').css("width", table_width*0.1 + "px");
+  $('th.record-no').css("width", table_width*0.1 + "px");
+  $('th.record-data').css("width", table_width*0.6 + "px");
+  $('th.record-count').css("width", table_width*0.1 + "px");
+}
 
 $(function() {
 
@@ -75,6 +103,10 @@ $(function() {
     }
   };
 
+});
+
+$(window).load(function(){
+  resize_windowful();
 });
 
 $(window).resize(function() {
