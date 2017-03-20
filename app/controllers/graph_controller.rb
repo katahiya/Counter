4a4f8319b129a6598ce15169aecf21b252c15fdd
katahiya <menoshitanoaka@gmail.com>
2017-03-20 18:35:09 +0900

    記録のグラフを表示

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,2 +1,24 @@
 class GraphController < ApplicationController
+  before_action -> {
+    logged_in_user(recorder_url(current_recorder))
+  }
+  before_action -> {
+    correct_user(parent_user.id)
+  }
+
+  def show
+    @data = @recorder.options.map do |option|
+      [option.name, option.records.count]
+    end.sort{|a, b| b[1] <=> a[1]}
+    get_modal_window
+  end
+
+  private
+    def current_recorder
+      @recorder = Recorder.find(params[:id])
+    end
+
+    def parent_user
+      @recorder.user
+    end
 end
