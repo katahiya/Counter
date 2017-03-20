67fb18318c456f2edb4660ae3cc4bc3842497b6a
katahiya <menoshitanoaka@gmail.com>
2017-03-20 23:50:46 +0900

    モーダルウィンドウの高さをウィンドウサイズに合わせて調整．ウィンドウサイズが480px以下になった時に消えるように変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,52 +17,6 @@
 //= require turbolinks
 //= require_tree .
 
-//コントローラーとアクションで呼び出しに制限をかける
-var filter = function(controller, action, callback) {
-  selector = `body[data-controller='${controller}'][data-action='${action}']`;
-  console.log(selector);
-  if($(selector).length > 0)
-    callback();
-};
-
-//resize windowful by window height
-var resize_windowful = function() {
-  console.log("resize");
-  var window_height = $(window).height();
-  var header_height = $('.navbar-fixed-top').height();
-  var resize = window_height - header_height - 50;
-  $('.windowful').css("max-height", resize + "px");
-  resize_option_bar(resize);
-  resize_records_table(resize + 30);
-};
-var resize_option_bar = function(base) {
-  if($('.option-bar').width() < 125){
-    $('.options-col').css("visibility", "hidden");
-    return;
-  }
-  $('.options-col').css("visibility", "visible");
-  var side_height = $('.option-bar-head').height();
-  $('.record_form').css("max-height", base-side_height + "px");
-}
-var resize_records_table = function(base) {
-  var head_height = $('.records-head').height();
-  var buttons_height = $('.records-buttons').height();
-  var thead_height = $('.records-thead').height();
-  var table_width = $('.records-tbody').width();
-  $('.records-tbody').css("max-height", base-head_height-buttons_height-thead_height + "px");
-  $('td.record-select').css("width", table_width*0.1 + "px");
-  $('td.record-action').css("width", table_width*0.1 + "px");
-  $('td.record-no').css("width", table_width*0.1 + "px");
-  $('td.record-data').css("width", table_width*0.6 + "px");
-  $('td.record-count').css("width", table_width*0.1 + "px");
-  table_width -= 20;
-  $('th.record-select').css("width", table_width*0.1 + "px");
-  $('th.record-action').css("width", table_width*0.1 + "px");
-  $('th.record-no').css("width", table_width*0.1 + "px");
-  $('th.record-data').css("width", table_width*0.6 + "px");
-  $('th.record-count').css("width", table_width*0.1 + "px");
-}
-
 $(function() {
   console.log("ready");
 
@@ -110,7 +64,6 @@ $(function() {
 
 $(document).on('turbolinks:load', function(){
   //resize section by window height
-  console.log("turbolinks");
   filter("recorders", "show", function() {
     resize_windowful()
   });
@@ -121,4 +74,72 @@ $(window).resize(function() {
   filter("recorders", "show", function() {
     resize_windowful()
   });
+  size_modal_window();
+  hide_less_than_min_modal();
 });
+
+//コントローラーとアクションで呼び出しに制限をかける
+var filter = function(controller, action, callback) {
+  selector = `body[data-controller='${controller}'][data-action='${action}']`;
+  if($(selector).length > 0)
+    callback();
+};
+
+//resize windowful by window height
+var resize_windowful = function() {
+  var window_height = $(window).height();
+  var header_height = $('.navbar-fixed-top').height();
+  var resize = window_height - header_height - 50;
+  $('.windowful').css("max-height", resize + "px");
+  resize_option_bar(resize);
+  resize_records_table(resize + 30);
+};
+var resize_option_bar = function(base) {
+  if($('.option-bar').width() < 125){
+    $('.options-col').css("visibility", "hidden");
+    return;
+  }
+  $('.options-col').css("visibility", "visible");
+  var side_height = $('.option-bar-head').height();
+  $('.record_form').css("max-height", base-side_height + "px");
+}
+var resize_records_table = function(base) {
+  var head_height = $('.records-head').height();
+  var buttons_height = $('.records-buttons').height();
+  var thead_height = $('.records-thead').height();
+  var table_width = $('.records-tbody').width();
+  $('.records-tbody').css("max-height", base-head_height-buttons_height-thead_height + "px");
+  $('td.record-select').css("width", table_width*0.1 + "px");
+  $('td.record-action').css("width", table_width*0.1 + "px");
+  $('td.record-no').css("width", table_width*0.1 + "px");
+  $('td.record-data').css("width", table_width*0.6 + "px");
+  $('td.record-count').css("width", table_width*0.1 + "px");
+  table_width -= 20;
+  $('th.record-select').css("width", table_width*0.1 + "px");
+  $('th.record-action').css("width", table_width*0.1 + "px");
+  $('th.record-no').css("width", table_width*0.1 + "px");
+  $('th.record-data').css("width", table_width*0.6 + "px");
+  $('th.record-count').css("width", table_width*0.1 + "px");
+}
+
+var size_modal_window = function() {
+  if('.modal-container'.length == 0) return;
+  if('.scroll-window'.length == 0) return;
+  var window_height = $(window).height();
+  var body_height = $('body').height();
+  var header_height = 41;
+  var buttons_height = 50;
+  var margin = window_height * 0.1;
+  var scroll_height = body_height*0.8 - header_height - buttons_height - margin*2
+  $('.modal-form').css("margin-top", margin + "px");
+  $('.scroll-window').css("max-height", scroll_height + "px");
+};
+
+var hide_less_than_min_modal = function() {
+  if('.modal-container'.length == 0) return;
+  var window_width = $(window).width();
+  if(window_width < 480){
+    $('.modal-container').modal("hide");
+    return;
+  }
+};
