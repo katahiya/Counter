a78372a581436fd22abbf52080e3a06d46ec2138
katahiya <menoshitanoaka@gmail.com>
2017-03-28 17:22:33 +0900

    不要なファイルを削除

--- oreset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm ./db/development.sqlite3
-rm ./db/test.sqlite3
-rails db:migrate
-rails db:migrate RAILS_ENV=test
--- x_server.sh
+++ x_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 Xvfb :1 -screen 0 1024x768x24 > /dev/null &
 firefox -display :1 -width 1024 -height 800 > /dev/null &
+sudo ln -s /usr/local/src/geckodriver/target/release/geckodriver  ~/.rbenv/shims/
