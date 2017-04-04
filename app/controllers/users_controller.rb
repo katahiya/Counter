6ea392fcefc2f18f44a3af3a544a01ce215a3667
katahiya <menoshitanoaka@gmail.com>
2017-04-04 18:47:10 +0900

    微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Your account successfully created!"
+      flash[:success] = "あなたのアカウントが作成されました!"
       redirect_to root_path
     else
       render 'new'
